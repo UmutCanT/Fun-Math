e0a71ede763c60b33272d8a82fc0c913d7c75286
UmutCanT <uctekcambusiness@gmail.com>
2021-04-03 21:10:33 +0300

    Character-core-movements
    Core movements by using keyboard

---
+++ Assets/Scripts/PlayerMoves.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoves : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    Animator animator;
+
+    Vector2 velocity;
+
+    //Player Attributes
+    [SerializeField]
+    float runSpeed = default;
+    [SerializeField]
+    float acceleration = default;
+    [SerializeField]
+    float deceleration = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyboardConroller();
+    }
+
+    /// <summary>
+    /// Controller for testing in Unity
+    /// </summary>
+    void KeyboardConroller()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        Vector2 scale = transform.localScale;
+
+        if(moveInput > 0)
+        {
+            //(current value, target value, delta)
+            velocity.x = Mathf.MoveTowards(velocity.x, moveInput * runSpeed, acceleration * Time.deltaTime);
+            animator.SetBool("Run", true);
+            scale.x = 0.3f;
+        }else if(moveInput < 0)
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, moveInput * runSpeed, acceleration * Time.deltaTime);
+            animator.SetBool("Run", true);
+            scale.x = -0.3f;
+        }
+        else
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
+            animator.SetBool("Run", false);
+        }
+        transform.localScale = scale;
+        transform.Translate(velocity * Time.deltaTime);
+    }
+}
