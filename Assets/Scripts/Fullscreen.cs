e78c998ebefd6dc209f94f1f73b183b5c0614190
UmutCanT <uctekcambusiness@gmail.com>
2021-04-03 15:44:24 +0300

    Background-for-any-res
    The background picture can scale to any resolution

--- Assets/Scripts/Fullscreen.cs
+++ Assets/Scripts/Fullscreen.cs
@@ -7,7 +7,18 @@ public class Fullscreen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //We use the sprite renderer component, to rearrange the size of the background image to the resolution 
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Vector2 tempScale = transform.localScale;
+
+        float bgWidth = spriteRenderer.size.x;
+
+        float screenHeight = Camera.main.orthographicSize * 2;
+        //Screen class is for taking device screen resolution values 
+        float screenWidth = screenHeight / Screen.height * Screen.width;
+
+        tempScale.x = screenWidth / bgWidth;
+        transform.localScale = tempScale;
     }
 
     // Update is called once per frame
