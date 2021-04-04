f3bffb3bca67c379b0539469def13354b2cc7e44
UmutCanT <uctekcambusiness@gmail.com>
2021-04-04 20:02:45 +0300

    Math-Problems-Initials

---
+++ Assets/Scripts/MathProblems.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//All values can be seen at the inspector
+[System.Serializable]
+public class MathProblems
+{
+    public enum MathOperations
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+
+    public float firstNumber;
+    public float secondNumber;
+    public float thirdNumber;
+    public float fourthNumber;
+    public MathOperations operation;
+    public float[] answers;
+
+    [Range(0, 3)]
+    public int correctAnswer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
